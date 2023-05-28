46abdc446e3ff8a096a1a64aab025b2fd009a3f6
Jaye <jayeli199@qq.com>
2023-05-28 13:05:41 +0800

    update style.

--- resource/js/markdown.js
+++ resource/js/markdown.js
@@ -255,7 +255,7 @@ $(function () {
                 break
             }
         }
-        spaces = spaces.replaceAll("&nbsp;&nbsp;&nbsp;&nbsp;", "&nbsp;&nbsp;")
+        // spaces = spaces.replaceAll("&nbsp;&nbsp;&nbsp;&nbsp;", "&nbsp;&nbsp;")
         if (isTableSymbol) {
             return spaces + html.trim().substr(i)
         } else {
